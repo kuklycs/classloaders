4a6b6bf0b8eed884e4076e038d9327f362936ea4
kuklycs <mato.kolesar23@gmail.com>
2023-03-28 03:31:58 +0200

    Change order of calls in Demo

--- composite-classloader/src/main/java/classloaders/Demo.java
+++ composite-classloader/src/main/java/classloaders/Demo.java
@@ -16,14 +16,15 @@ public class Demo {
         //FIXME first mistake, loading all classes for all versions to invoke Class.forName and initialize static initializers
         compositeClassLoader.loadClass("model.Person");
 
-        compositeClassLoader.setVersion(PrintVersion.V2);
-
-        compositeClassLoader.loadClass("model.Person");
-
-
         // Now this is a result which I wanted to achieve but not nice yet
         Printable first = PrintableFactory.getPrintableImpl(PrintVersion.V1, "Martin", 27);
         System.out.println(first.getAsString());
+
+
+        compositeClassLoader.setVersion(PrintVersion.V2);
+
+        //have to load class for PrintVersion V2 otherwise will fail
+        compositeClassLoader.loadClass("model.Person");
 
         Printable second = PrintableFactory.getPrintableImpl(PrintVersion.V2, "Honza", 40);
         System.out.println(second.getAsString());
