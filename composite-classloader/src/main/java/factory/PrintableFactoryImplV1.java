858eb2d2959067ee96dbe8cc157a5b6d8b84fa49
kuklycs <mato.kolesar23@gmail.com>
2023-04-18 23:51:48 +0200

    try to refactor PrintableFactoryV1

--- composite-classloader/src/main/java/factory/PrintableFactoryImplV1.java
+++ composite-classloader/src/main/java/factory/PrintableFactoryImplV1.java
@@ -10,17 +10,28 @@ import java.net.URL;
 import java.net.URLClassLoader;
 
 public class PrintableFactoryImplV1 implements PrintableFactory {
+    private Constructor<?> loadedConstructor;
+
     @Override
     public Printable create(String name, int age) throws CreationException {
         System.out.println("Call create() on factoryV1");
-        URL[] resource = LazyResourceLoader.getFirstResourceOnDemand();
+        if (loadedConstructor == null) {
+            URL[] resource = LazyResourceLoader.getFirstResourceOnDemand();
+            loadedConstructor = getConstructor(resource);
+        }
 
+        try {
+            return (Printable) loadedConstructor.newInstance(name, age);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new CreationException("Could not create Printable instance", e);
+        }
+    }
+
+    private Constructor<?> getConstructor(URL[] resource) throws CreationException {
         try (URLClassLoader classLoader = new URLClassLoader(resource)) {
             Class<?> clz = classLoader.loadClass("model.Person");
-            Constructor<?> constructor = clz.getConstructor(String.class, Integer.TYPE);
-            return (Printable) constructor.newInstance(name, age);
-        } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | InstantiationException |
-                 IllegalAccessException | IOException e) {
+            return clz.getConstructor(String.class, Integer.TYPE);
+        } catch (ClassNotFoundException | NoSuchMethodException | IOException e) {
             throw new CreationException("Could not create Printable instance", e);
         }
     }
